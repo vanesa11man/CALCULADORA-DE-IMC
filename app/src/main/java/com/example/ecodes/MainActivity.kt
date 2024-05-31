9887dc39f28780d7a404b7aebda470f3ec614528
vanesa11man <vanesamendozayu11@gmail.com>
2024-05-31 18:27:44 -0500

    Proyecto IMC

---
+++ app/src/main/java/com/example/ecodes/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.ecodes
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val etWeight = findViewById<EditText>(R.id.etWeight)
+        val etHeight = findViewById<EditText>(R.id.etHeight)
+        val btnCalculate = findViewById<Button>(R.id.btnCalculate)
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+
+        btnCalculate.setOnClickListener {
+            val weight = etWeight.text.toString().toDoubleOrNull()
+            val height = etHeight.text.toString().toDoubleOrNull()
+
+            if (weight != null && height != null && height > 0) {
+                val bmi = weight / (height * height)
+                tvResult.text = "Tu IMC es %.2f".format(bmi)
+            } else {
+                tvResult.text = "Por favor, ingresa valores válidos"
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
